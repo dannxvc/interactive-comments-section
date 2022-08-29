5acb199117a0b418e7948eb780c0bea526d8d1fe
danna <dannavila.1809@gmail.com>
2022-08-29 16:38:48 -0500

    Add deleteReply reductor

--- src/components/shared/Modal.js
+++ src/components/shared/Modal.js
@@ -1,18 +1,20 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { Context } from "../../services/Memory";
 import '../../assets/css/Modal.css';
 
-function Modal({classModal,id,handleCancelClick}) {
+function Modal({classModal,id,parentId,rootid,handleCancelClick}) {
 
     const [, dispatch] = useContext(Context);
+
     const deleteComment = (e) => {
         e.preventDefault();
         dispatch({type: 'deleteComment', id:id});
     }
-
-    useEffect(() => {
-    },[id]);
-
+    
+    const deleteReply= (e) => {
+        e.preventDefault();
+        dispatch({type: 'deleteReply', parentId: parentId, rootid:rootid, idActual:id});
+    }
     return ( 
         <div className={`modal ${classModal}`}>
             <div className="modal-alert">
@@ -22,7 +24,7 @@ function Modal({classModal,id,handleCancelClick}) {
                 <p className="modal-description">Are you sure you want to delete this comment? This will remove the comment and can't be undone.</p>
                 <div className="modal-btns">
                     <button className="btn-modal btn-gray" onClick={handleCancelClick}>NO, CANCEL</button>
-                    <button className="btn-modal btn-red" onClick={deleteComment}>YES, DELETE</button>
+                    <button className="btn-modal btn-red" onClick={parentId?deleteReply:deleteComment}>YES, DELETE</button>
                 </div>
             </div>
         </div>
